a06beaa4e5877b33a29499f41ea89196d9109885
numme <nummer14@naver.com>
2025-06-20 13:45:38 +0900

    Fourth Commit

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,29 @@
-class MyRunnable implements Runnable {
+class SumRunnable implements Runnable {
+    private final int[] numbers;
+
+    public SumRunnable(int[] numbers) {
+        this.numbers = numbers;
+    }
+
     @Override
     public void run() {
-        System.out.println("Runnable 실행 중");
+        int sum = 0;
+
+        for(int n: numbers) {
+            sum += n;
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println("Thread interrupted.");
+            }
+        }
+
+        System.out.println(Thread.currentThread().getName() + " - Sum: " + sum);
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        Thread thread = new Thread(new MyRunnable());
-
-        thread.start();
     }
 }
