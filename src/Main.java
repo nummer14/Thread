d3106d2cd109306a29f865d22a0888b6ce06ac53
numme <nummer14@naver.com>
2025-06-23 11:46:46 +0900

    Eighth Commit

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,20 @@
-class SumThread extends Thread {
-    private int[] array;
-    private int start, end;
-    private int partialSum = 0;
-
-    public SumThread(int[] array, int start, int end) {
-        this.array = array;
-        this.start = start;
-        this.end = end;
+class Animal {
+    public void sound() {
+        System.out.println("Animal makes a sound.");
     }
+}
 
+class Dog extends Animal {
     @Override
-    public void run() {
-        for(int i = start; i < end; i++) {
-            partialSum += array[i];
-        }
-    }
-
-    public int getPartialSum() {
-        return partialSum;
+    public void sound() {
+        System.out.println("Dog barks.");
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = new int[100];
+        Animal dog = new Dog();
 
-        for(int i = 0; i < numbers.length; i++) {
-            numbers[i] = i + 1;
-        }
-
-        int numThreads = 4;
-        int chunkSize = numbers.length / numThreads;
-
-        SumThread[] threads = new SumThread[numThreads];
-
-        for (int i = 0; i < numThreads; i++) {
-            int start = i * chunkSize;
-            int end = (i == numThreads - 1) ? numbers.length : start;
-
-            threads[i] = new SumThread(numbers, start, end);
-            threads[i].start();
-        }
+        dog.sound();
     }
 }
